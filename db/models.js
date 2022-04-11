58eec18a62a8a9e992cad42925dc0bb36aeeba95
renehu <19586178+renehu@users.noreply.github.com>
2022-04-11 16:09:40 +0800

    Update models.js

--- db/models.js
+++ db/models.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/MongoDB')
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/MongoDB");
 const connection = mongoose.connection;
-connection.on('connected', ()=>{
-    console.log('DB connected!')
-})
+connection.on("connected", () => {
+  console.log("DB connected!");
+});
 
 const userSchema = mongoose.Schema({
-    username:{type:String,required:true},
-    password:{type:String,required:true},
-    type:{type:String,required:true},
-    avatar:{type:String},
-    bio:{type:String},
-    position:{type:String},
-    company:{type:String},
-    salary:{type:String},
-    location:{type:String}
-})
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  type: { type: String, required: true },
+  avatar: { type: String },
+  position: { type: String },
+  company: { type: String },
+  salary: { type: String },
+  location: { type: String },
+  info: { type: String },
+});
 
-const UserModel = mongoose.model('user', userSchema)
+const UserModel = mongoose.model("user", userSchema);
 
-exports.UserModel = UserModel
+exports.UserModel = UserModel;
