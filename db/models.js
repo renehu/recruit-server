d374ee63317d449a92380e28551deae49bff2629
renehu <rene.hu@outlook.com>
2024-08-28 14:57:02 +0800

    add mongoURI

--- db/models.js
+++ db/models.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/MongoDB");
+mongoose.set("strictQuery", false);
+const config = require("./config");
+//mongoose.connect("mongodb://localhost:27017/MongoDB");
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 const logger = require("../utils/logger");
 
